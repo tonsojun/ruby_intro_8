e3bb530018f3b713b2a090788f542221061ab4de
tonsojun <josephjun@gmail.com>
2021-03-03 16:04:55 -0800

    Completed ruby intro assign 8, subtract and double button working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,16 @@ class PagesController < ApplicationController
     redirect_to '/'
   end
 
+  def subtract
+    session[:number] = 1 if !session[:number]
+    session[:number] -= 1
+    redirect_to '/'
+  end
+
+  def double
+    session[:number] = 1 if !session[:number]
+    session[:number] += session[:number]
+    redirect_to '/'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#main'
 
   post 'increase', to: 'pages#increase'
+
+  post 'subtract', to: 'pages#subtract'
+
+  post 'double', to: 'pages#double'
 end
